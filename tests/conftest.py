fc2bed33cc44be1f800c4ee2ef033a6eeba1d7cd
mrowacz <mrowacz@gmail.com>
2024-09-11 23:06:08 +0200

    tests: Refactor s3 reading example

---
+++ tests/conftest.py
@@ -0,0 +1,69 @@
+import os
+import signal
+import subprocess
+from pathlib import Path
+
+import boto3
+import pytest
+from moto import mock_aws
+from pyspark.sql import SparkSession
+
+BUCKET_NAME = "test-bucket"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def moto_endpoint():
+    print("Set Up Moto Server")
+    process = subprocess.Popen(
+        "moto_server", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid
+    )
+
+    yield "http://127.0.0.1:5000"
+    print("Tear Down Moto Server")
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+
+
+@pytest.fixture(autouse=True, scope="session")
+def spark(moto_endpoint):
+
+    os.environ["SPARK_LOCAL_IP"] = "localhost"
+    os.environ["PYSPARK_SUBMIT_ARGS"] = (
+        '--packages "org.apache.hadoop:hadoop-aws:3.3.4" pyspark-shell'
+    )
+
+    spark = SparkSession.builder.config(
+        map={
+            "spark.hadoop.fs.s3.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
+            "spark.hadoop.fs.s3a.access.key": "foo",
+            "spark.hadoop.fs.s3a.secret.key": "foo",
+            "spark.hadoop.fs.s3a.endpoint": moto_endpoint,
+        },
+    ).getOrCreate()
+    yield spark
+
+
+@pytest.fixture(autouse=True, scope="session")
+def test_s3_bucket_name(moto_endpoint) -> str:
+    mock_aws().start()
+    s3_connection = boto3.resource(
+        "s3", region_name="us-east-1", endpoint_url=moto_endpoint
+    )
+    s3_connection.create_bucket(Bucket=BUCKET_NAME)
+
+    s3_connection = boto3.resource(
+        "s3", region_name="us-east-1", endpoint_url=moto_endpoint
+    )
+
+    local_directory = Path(__file__).parent / "fixtures"
+    bucket = s3_connection.Bucket(BUCKET_NAME)
+
+    for root, dirs, files in os.walk(local_directory):
+        for file in files:
+            local_path = os.path.join(root, file)
+            relative_path = os.path.relpath(local_path, local_directory)
+            s3_path = relative_path.replace(
+                "\\", "/"
+            )  # Ensure S3 path uses forward slashes
+            bucket.upload_file(local_path, s3_path)
+    mock_aws().stop()
+    yield BUCKET_NAME
--- tests/test_pyspark.py
+++ tests/test_pyspark.py
@@ -1,56 +1,16 @@
-import boto3
-import os
-import pytest
-import signal
-import subprocess
-
 from moto import mock_aws
-from pyspark.sql import SparkSession
-from pyspark.sql import SparkSession
-from pyspark.sql import Row
-
-@pytest.fixture(autouse=True, scope="module")
-def handle_server():
-    print("Set Up Moto Server")
-    process = subprocess.Popen("moto_server",
-                               stdout=subprocess.PIPE,
-                               shell=True,
-                               preexec_fn=os.setsid)
-
-    yield
-    print("Tear Down Moto Server")
-    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+from pyspark.sql import Row, SparkSession
 
 
 @mock_aws
-def test_clean_user(handle_server):
+def test_spark(spark: SparkSession, test_s3_bucket_name: str):
 
-    os.environ["SPARK_LOCAL_IP"] = "localhost"
-    os.environ["PYSPARK_SUBMIT_ARGS"] = (
-        '--packages "org.apache.hadoop:hadoop-aws:3.3.4" pyspark-shell'
+    expected_data = [
+        Row(name="Alice", age=30),
+        Row(name="Bob", age=25),
+        Row(name="Cathy", age=27),
+    ]
+    actual_result = spark.read.parquet(
+        f"s3://{test_s3_bucket_name}/simple_data.parquet"
     )
-    s3_connection = boto3.resource('s3',
-                                   region_name='us-east-1',
-                                   endpoint_url="http://127.0.0.1:5000")
-    s3_connection.create_bucket(Bucket='twitter-user')
-
-    spark = SparkSession.builder.config("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem").getOrCreate()
-
-    hadoop_conf = spark._jsc.hadoopConfiguration()
-    hadoop_conf.set(
-        "fs.s3a.endpoint",
-        f"http://127.0.0.1:5000"
-    )
-    hadoop_conf.set(
-        "fs.s3a.access.key",
-        ""
-    )
-    hadoop_conf.set(
-        "fs.s3a.secret.key",
-        ""
-    )
-
-    data = [Row(name="Alice", age=30), Row(name="Bob", age=25), Row(name="Cathy", age=27)]
-    df = spark.createDataFrame(data)
-    df.write.csv("s3a://twitter-user/test_user_data.csv")
-    df.write.parquet("s3://twitter-user/test_user_data.parquet")
+    assert sorted(actual_result.collect(), key=lambda row: row["name"]) == expected_data
